78f9391c83b6bc37ac9d98d6a93d77b5c698104f
mcharkviani <m_charkviani@cu.edu.ge>
2018-11-02 14:00:21 +0400

    Add project files.

---
+++ MobileApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MobileApplication.Startup))]
+namespace MobileApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
